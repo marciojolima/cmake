b9eea6f3b9e777ba95a4f2d5b0554e794d0163e6
Marcio <marciocad@gmail.com>
2024-10-20 09:31:23 -0300

    clang-format for format code

---
+++ Ep020/cmake/ClangFormat.cmake
@@ -0,0 +1,27 @@
+# Define a function to create a custom target for running clang-format
+function(add_clang_format_target TARGET_NAME SOURCE_DIR)
+    #search for clang-format automaticly
+    #find_program(CLANG-FORMAT_PATH clang-format REQUIRED)
+    #set manually
+    set(CLANG-FORMAT_PATH "C:/compilers/llvm/clang_llvm-18.1.7-x86_64-pc-windows-msvc/bin/clang-format.exe")
+
+    # Find all C++ source files in the specified directory
+    file(GLOB_RECURSE FORMAT_SOURCES
+        LIST_DIRECTORIES false
+        "${SOURCE_DIR}/*.cpp"
+        "${SOURCE_DIR}/*.h"
+    )
+
+    message("CLANG-FORMAT_PATH: " ${CLANG-FORMAT_PATH})
+    message("FORMAT_SOURCES: " ${FORMAT_SOURCES})
+    message("SOURCE_DIR: " ${SOURCE_DIR})
+    message("CMAKE_SOURCE_DIR: " ${CMAKE_SOURCE_DIR})
+    # Create a custom target to run clang-format
+
+    add_custom_target(${TARGET_NAME}
+        COMMAND ${CLANG-FORMAT_PATH} -i ${FORMAT_SOURCES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}  # Change this if needed
+        COMMENT "Running clang-format on ${SOURCE_DIR} sources"
+    )
+
+endfunction()
